bcbad0fba8a6612e3da96e994e4acf4d80b1a351
H0lale <maurom01@outlook.com>
2023-07-20 21:12:11 -0500

    initial

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:997229281124:web:a6c229a395e57d8c594471";
+  static String apiKey = "AIzaSyAUdXnd50dJsNMSsKIt58dmJVdDTg-5400";
+  static String messagingSenderId = "997229281124";
+  static String projectId = "chatappflutter-38078";
+}
