827bd9627b98bbd2a36eacfb06b00bef59d7f1e2
Juan Diego Castillo Carnero <juandiegomaster13@hotmail.com>
2023-07-21 21:03:01 -0500

    ahora tenemos más info sobre los grupos

--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -56,4 +56,23 @@ class DatabaseService {
           FieldValue.arrayUnion(["${groupDocumentReference.id}_$groupName"])
     });
   }
+
+//obtener los chats
+  getChats(String groupId) async {
+    return groupCollection
+        .doc(groupId)
+        .collection("messages")
+        .orderBy("time")
+        .snapshots();
+  }
+
+  Future getGroupAdmin(String groupId) async {
+    DocumentReference d = groupCollection.doc(groupId);
+    DocumentSnapshot documentSnapshot = await d.get();
+    return documentSnapshot['admin'];
+  }
+
+  getGroupMembers(groupId) async {
+    return groupCollection.doc(groupId).snapshots();
+  }
 }
