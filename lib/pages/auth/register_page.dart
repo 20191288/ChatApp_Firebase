bbd60d8725c7b0baa37f8c3843550fda97c51029
Juan Diego Castillo Carnero <juandiegomaster13@hotmail.com>
2023-07-21 23:50:56 -0500

    oe Micali, tenias un bug en el register pero ya lo arreglé

--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -1,3 +1,4 @@
+import 'package:chatapp_firebase/pages/auth/login_page.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
@@ -23,150 +24,147 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
-      ),
-      body: _isLoading
-        ? Center(
-          child: CircularProgressIndicator(
-            color: Theme.of(context).primaryColor )
-        ) : SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  "Whatsapp 2",
-                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                  ),
-                const SizedBox(height: 10),
-                const Text(
-                  "Crea tu cuenta y vende tu alma al diablo!",
-                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400)
-                  ),
-                Image.asset("assets/register.jpg"),
-                const SizedBox(height: 25),
-                TextFormField(
-                  decoration: textInputDecoration.copyWith(
-                      labelText: "Nombre completo",
-                      prefixIcon: Icon(
-                        Icons.person,
-                        color: Theme.of(context).primaryColor,
-                      )),
-                  onChanged: (val) {
-                    setState(() {
-                      fullName = val;
-                    });
-                  },
-                  validator: (val) {
-                    if (val!.isNotEmpty) {
-                      return null;
-                    } else {
-                      return "Nombre no puede ser vacío";
-                    }
-                  },
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                TextFormField(
-                  decoration: textInputDecoration.copyWith(
-                    labelText: "Email",
-                    prefixIcon: Icon(
-                      Icons.email,
-                      color: Theme.of(context).primaryColor,
-                    )
-                  ),
-                  onChanged: (val) {
-                    setState(() {
-                      email = val;
-                    });
-                  },
-
-                  validator: (val) {
-                    return RegExp(
-                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                            .hasMatch(val!)
-                        ? null
-                        : "Por favor ingrese un correo electrónico válido";
-                  },
-                ),
-                const SizedBox(height: 15),
-                TextFormField(
-                  obscureText: true,
-                  decoration: textInputDecoration.copyWith(
-                    labelText: "Contraseña",
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Theme.of(context).primaryColor,
-                    )
-                  ),
-                  onChanged: (val) {
-                    setState(() {
-                      password = val;
-                    });
-                  },
-                  validator: (val) {
-                    if (val!.length < 6) {
-                      return "La contraseña debe ser mayor a 6 caractéres";
-                    } else {
-                      return null;
-                    }
-                  },
-                ),
-
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Theme.of(context).primaryColor,
-                        elevation: 0,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30))),
-                    child: const Text(
-                      "Registrarse",
-                      style:
-                          TextStyle(color: Colors.white, fontSize: 16),
-                    ),
-                    onPressed: () {
-                      register();
-                    },
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text.rich(TextSpan(
-                  text: "Ya creaste tu cuenta? ",
-                  style: const TextStyle(
-                      color: Colors.black, fontSize: 14),
-                  children: <TextSpan>[
-                    TextSpan(
-                        text: "Ingresa aquí",
-                        style: const TextStyle(
-                            color: Colors.black,
-                            decoration: TextDecoration.underline),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            nextScreen(context, const RegisterPage());
-                          }),
-                  ],
-                )),
-              ],
-            ),
-          ),
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
         ),
-      )
-    );
+        body: _isLoading
+            ? Center(
+                child: CircularProgressIndicator(
+                    color: Theme.of(context).primaryColor))
+            : SingleChildScrollView(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Whatsapp 2",
+                          style: TextStyle(
+                              fontSize: 40, fontWeight: FontWeight.bold),
+                        ),
+                        const SizedBox(height: 10),
+                        const Text("Crea tu cuenta y vende tu alma al diablo!",
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.w400)),
+                        Image.asset("assets/register.jpg"),
+                        const SizedBox(height: 25),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                              labelText: "Nombre completo",
+                              prefixIcon: Icon(
+                                Icons.person,
+                                color: Theme.of(context).primaryColor,
+                              )),
+                          onChanged: (val) {
+                            setState(() {
+                              fullName = val;
+                            });
+                          },
+                          validator: (val) {
+                            if (val!.isNotEmpty) {
+                              return null;
+                            } else {
+                              return "Nombre no puede ser vacío";
+                            }
+                          },
+                        ),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        TextFormField(
+                          decoration: textInputDecoration.copyWith(
+                              labelText: "Email",
+                              prefixIcon: Icon(
+                                Icons.email,
+                                color: Theme.of(context).primaryColor,
+                              )),
+                          onChanged: (val) {
+                            setState(() {
+                              email = val;
+                            });
+                          },
+                          validator: (val) {
+                            return RegExp(
+                                        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                    .hasMatch(val!)
+                                ? null
+                                : "Por favor ingrese un correo electrónico válido";
+                          },
+                        ),
+                        const SizedBox(height: 15),
+                        TextFormField(
+                          obscureText: true,
+                          decoration: textInputDecoration.copyWith(
+                              labelText: "Contraseña",
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: Theme.of(context).primaryColor,
+                              )),
+                          onChanged: (val) {
+                            setState(() {
+                              password = val;
+                            });
+                          },
+                          validator: (val) {
+                            if (val!.length < 6) {
+                              return "La contraseña debe ser mayor a 6 caractéres";
+                            } else {
+                              return null;
+                            }
+                          },
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Theme.of(context).primaryColor,
+                                elevation: 0,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30))),
+                            child: const Text(
+                              "Registrarse",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 16),
+                            ),
+                            onPressed: () {
+                              register();
+                            },
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text.rich(TextSpan(
+                          text: "Ya creaste tu cuenta? ",
+                          style: const TextStyle(
+                              color: Colors.black, fontSize: 14),
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: "Ingresa aquí",
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    decoration: TextDecoration.underline),
+                                recognizer: TapGestureRecognizer()
+                                  ..onTap = () {
+                                    nextScreen(context, const LoginPage());
+                                  }),
+                          ],
+                        )),
+                      ],
+                    ),
+                  ),
+                ),
+              ));
   }
- register() async {
+
+  register() async {
     if (formKey.currentState!.validate()) {
       setState(() {
         _isLoading = true;
@@ -187,5 +185,5 @@ class _RegisterPageState extends State<RegisterPage> {
         }
       });
     }
- }
+  }
 }
